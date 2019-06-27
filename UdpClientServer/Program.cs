6e751d82c9f1a5087b48c9719df60e092717c6a8
samurrai <samurrrai.r@gmail.com>
2019-06-27 14:10:55 +0600

    initial

---
+++ UdpClientServer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Design;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace UdpClientServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+
+            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+
+            printscreen.Save("printscreen.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+
+
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            serverSocket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345));
+
+            using (var reader = new StreamReader("printscreen.jpg"))
+            {
+                string content = reader.ReadToEnd();
+                byte[] buffer = Encoding.Default.GetBytes(content);
+                while (true)
+                {
+                    EndPoint clientEndPoint = new IPEndPoint(0, 0);
+                    byte[] receiveBuffer = new byte[1024 * 64];
+                    int receiveSize = serverSocket.ReceiveFrom(receiveBuffer, ref clientEndPoint);
+                    if (receiveSize == 0)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine(Encoding.UTF8.GetString(receiveBuffer));
+                    serverSocket.SendTo(buffer, clientEndPoint);
+                }
+            }
+        }
+    }
+}
---
+++ UdpProtocol/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UdpProtocol
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            string ip = "127.0.0.1";
+            int port = 12345;
+
+            EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            string text = "Hello UDP";
+            clientSocket.SendTo(Encoding.UTF8.GetBytes(text), remoteEndPoint);
+
+            byte[] buffer = new byte[64 * 1024];
+            clientSocket.ReceiveFrom(buffer, ref remoteEndPoint);
+        }
+    }
+}
